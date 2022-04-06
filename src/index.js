20f96e783cafe42d28e5620d35db5fa2989c05c0
mtrucc <mtrucc@qq.com>
2022-04-06 13:24:45 +0800

    提交

--- src/index.js
+++ src/index.js
@@ -1,14 +1,122 @@
-import bug from './bug'
+const onPrintPdfJS = async (array) => {
+  if (typeof window !== 'undefined') {
+    // browser code
+    let { default: printJS } = await import('print-js');
 
-function component() {
-  const element = document.createElement('div');
+    printJS({
+      printable: array,
+      type: 'image',
+    });
+  }
+};
 
-  // Lodash, currently included via a script, is required for this line to work
-  // Lodash, now imported by this script
-  element.innerHTML = 'Hello Bug'
+async function PrintPdf(url, wokerUrl) {
+  console.log('url', url);
+  let printList = [];
+  var pdfJS = window['pdfjs-dist/build/pdf'];
+  console.log('window.location.origin', window.location.origin)
+  pdfJS.GlobalWorkerOptions.workerSrc = wokerUrl;
+  const pdf = await pdfJS.getDocument(url).promise;
+  const page = await pdf.getPage(1);
+  const scale = 10;
+  const viewport = page.getViewport({
+    scale,
+  });
+  const canvas = document.createElement('CANVAS');
+  const canvasContext = canvas.getContext('2d');
+  canvas.height = viewport.height;
+  canvas.width = viewport.width;
+  const renderContext = {
+    canvasContext,
+    viewport,
+  };
+  await page.render(renderContext).promise.then((data) => {
+    const newCanvas = document.createElement('canvas');
+    newCanvas.width = 530 * scale;
+    newCanvas.height = 200 * scale;
+    const newCanvasContext = newCanvas.getContext('2d');
+    newCanvasContext.drawImage(
+      canvas,
+      20 * scale,
+      210 * scale,
+      550 * scale,
+      190 * scale,
+      0,
+      0,
+      530 * scale,
+      190 * scale
+    );
+    const dataURL = newCanvas.toDataURL('image/png', 1);
+    printList.push(dataURL);
+    return dataURL;
+  });
 
-  console.log(`bug`, bug(789))
-  return element;
+  const page2 = await pdf.getPage(2);
+  const viewport2 = page2.getViewport({
+    scale: 2,
+  });
+  const canvas2 = document.createElement('CANVAS');
+  const canvasContext2 = canvas2.getContext('2d');
+  canvas2.height = viewport2.height;
+  canvas2.width = 1060;
+
+  const renderContext2 = {
+    canvasContext: canvasContext2,
+    viewport: viewport2,
+  };
+
+  const operators = await page2.getOperatorList();
+
+  const rawImgOperator = operators.fnArray
+    .map((f, index) => (f === pdfJS.OPS.paintImageXObject ? index : null))
+    .filter((n) => n !== null);
+
+  const filename = operators.argsArray[rawImgOperator[1]][0];
+
+  page2.objs.get(filename, async (arg) => {
+    const canvas = document.createElement('canvas');
+    canvas.width = arg.width;
+    canvas.height = arg.height;
+    const ctx = canvas.getContext('2d');
+
+    const data = new Uint8ClampedArray(arg.width * arg.height * 4);
+    let k = 0;
+    let i = 0;
+    while (i < arg.data.length) {
+      data[k] = arg.data[i];
+      data[k + 1] = arg.data[i + 1];
+      data[k + 2] = arg.data[i + 2];
+      data[k + 3] = 255;
+
+      i += 3;
+      k += 4;
+    }
+    const imgData = ctx.createImageData(arg.width, arg.height);
+    imgData.data.set(data);
+    ctx.putImageData(imgData, 0, 0);
+    const newCanvas = document.createElement('canvas');
+    newCanvas.width = (arg.height * 1000) / 665;
+    newCanvas.height = arg.height;
+    const newCanvasContext = newCanvas.getContext('2d');
+    newCanvasContext.drawImage(
+      canvas,
+      0,
+      0,
+      newCanvas.width,
+      newCanvas.height,
+      0,
+      0,
+      newCanvas.width,
+      newCanvas.height
+    );
+
+    const dataURL = newCanvas.toDataURL('image/png', 1);
+    printList.push(dataURL);
+  });
+
+  onPrintPdfJS(printList);
 }
 
-document.body.appendChild(component());
+// PrintPdf('test.pdf', window.location.origin + '/dist/pdf.worker.js');
+
+window.PrintPdf = PrintPdf;
--- src/test.js
+++
@@ -1,5 +0,0 @@
-export default {
-  a:123,
-  b:234,
-  c:345
-}
